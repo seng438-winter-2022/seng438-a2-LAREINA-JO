8afa13d692eeb62cba1f8da16df4964b69998b7a
zheng-628 <zheng.chen1@ucalgary.ca>
2022-02-09 12:06:34 -0700

    Test Files

---
+++ DataUtilitiesTest.java
@@ -0,0 +1,14 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.DataUtilities;
+import org.jfree.data.Values2D;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.Test;
+
+public class DataUtilitiesTest {
+	 
+
+}
---
+++ RangeTest.java
@@ -0,0 +1,10 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*; 
+
+import org.jfree.data.Range; 
+import org.junit.*;
+
+public class RangeTest {
+
+}
